5dc9a4e2eae06e8996f106683286b2fff26cc21a
MoonJaeSeung <wotmdskfk32@naver.com>
2022-12-26 09:44:03 +0900

    영화 리스트 추가폼 만들기

--- ex1/src/App.js
+++ ex1/src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState,useEffect} from 'react';
+import Movie from './components/Movie';
 
 function App() {
+
+  const [movieTitle, setMovieTitle] = useState('');
+  const [movieYear, setMovieYear] = useState('');
+  const [movies,setMovies] = useState([
+    {title: 'kossie coder1',year:2001},
+    {title: 'kossie coder2',year:2002},
+    {title: 'kossie coder3',year:2003},
+    {title: 'kossie coder4',year:2004},
+  ])
+
+  //state가 변경이 되어야만 렌더링이 된다.
+  useEffect(() =>{
+    console.log('render');
+  })
+
+    const renderMovies = movies.map(movie =>{
+      return(
+       <Movie movie={movie} key={movie.title} />
+      );
+    });
+    const addMovie = (event) =>{
+      event.preventDefault();
+      setMovies([
+        ...movies,
+        {
+          title : movieTitle,
+          year: movieYear,
+        }
+      ])
+     
+    };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Movie list</h1>
+     <form onSubmit={addMovie}>
+      <input
+      type="text"
+      value={movieTitle}
+      placeholder="영화 제목"
+      onChange={e => setMovieTitle(e.target.value)}
+      /><br />
+      <input
+      type="text"
+      value={movieYear}
+      placeholder="개봉년도"
+      onChange={e => setMovieYear(e.target.value)}
+      /><br />
+      <button>영화 추가</button>
+     </form>
+     {renderMovies}
     </div>
   );
 }
